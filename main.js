d394f756cfa6a99b2b1ab34bad42c2ee24757b35
Anupam <97234085+AnupamSwami@users.noreply.github.com>
2023-12-13 18:24:55 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,20 @@
 function setup() {
+    video = createCapture(VIDEO) ;
+    video.size(550 , 500) ;
+
     canvas = createCanvas(500 , 470) ;
-    canvas.position(850 , 95) ;
+    canvas.position(800 , 90) ;
+
+    poseNet = ml5.poseNet(video , modelLoaded) ;
+    poseNet.on('pose' , gotPoses) ;
 } 
+
+function modelLoaded() {
+    console.log('PoseNet nis Initialised.') ;
+}
+
+function gotPoses(results) {
+    if(results.length > 0) {
+        console.log(results) ;
+    }
+}
